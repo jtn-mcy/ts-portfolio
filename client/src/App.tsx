a1b3f21f2f8bfa86ff6b9c2bd3bc5018e076ec6e
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-04-30 18:22:56 -0700

    starter files

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/_global.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        PORTFOLIO
+      </header>
+    </div>
+  );
+}
+
+export default App;
