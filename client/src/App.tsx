f0d0e9a70c5e359a70b1f099cf92e573a1381a85
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-04-30 18:32:42 -0700

    add antd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './styles/_global.scss'
+import { PageHeader } from 'antd'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        PORTFOLIO
-      </header>
+      <PageHeader title='PORTFOLIO'>
+      </PageHeader>
     </div>
   );
 }
