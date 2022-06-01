b445dabcd754ae8f1518d0118b8bcf0252508a77
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-01 12:12:00 -0700

    add unsplash to home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,21 +6,25 @@ import Layout from './layout';
 import Breadcrumbs from './components/Breadcrumbs';
 import Home from './pages/Home';
 import About from './pages/About';
+import { queryClient } from './api';
+import { QueryClientProvider } from 'react-query';
 
 const App: React.FC = () => {
   return (
-    <Layout>
-      <Breadcrumbs params={[
-        //getParams here
-      ]} />
-      <PageHeader title='PORTFOLIO' >
-      </PageHeader>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Breadcrumbs params={[
+          //getParams here
+        ]} />
+        <PageHeader title='PORTFOLIO' >
+        </PageHeader>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GetUnsplashImg } from '../../api'
+
 
 
 const Home: React.FC = () => {
+  const [background, setBackground] = useState<string>();
+  const { data: images } = GetUnsplashImg();
+
+  const getRandomImage: (images: string[]) => string = images => {
+    return images[Math.floor(Math.random() * images.length)]
+  }
+
+  useEffect(() => {
+    if (images) setBackground(getRandomImage(images))
+  }, [images])
+
+  const style = {
+    backgroundImage: `url(${background})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: "cover",
+    height: '100vh'
+  }
+
   return (
-    <div>Home</div>
+    <div style={style}></div>
   )
 }
 
