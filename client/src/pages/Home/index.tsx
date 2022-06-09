dd5716123c10cbafbac8c0fe2e964702b4fe10bb
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-08 17:08:22 -0700

    add card and centering

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { GetUnsplashImg } from '../../api'
 import placeholder from '../../assets/placeholderBG.png'
-
+import HomeTitle from '../../components/HomeTitle'
+import styles from './index.module.scss'
 
 const Home: React.FC = () => {
   const [background, setBackground] = useState<string>();
@@ -19,11 +20,12 @@ const Home: React.FC = () => {
     backgroundImage: `url(${background ? background : placeholder})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: "cover",
-    height: '100%'
   }
 
   return (
-    <div style={style}></div>
+    <div className={styles.CenterCard} style={style}>
+      <HomeTitle />
+    </div>
   )
 }
 
