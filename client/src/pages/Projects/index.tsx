a2e87b2ef360ca068b96bc3e0912b88cef65c9c8
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-09 17:06:23 -0700

    add test descriptors

--- client/src/pages/Projects/index.tsx
+++ client/src/pages/Projects/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import { Project } from '../../components/SiderProjects';
-import { Col, Row, Carousel } from 'antd';
+import { Col, Row, Carousel, Descriptions, Badge } from 'antd';
 import styles from './index.module.scss'
+
+const ProjectDescriptions: React.FC = () => {
+  return (
+    <Descriptions title="Project 1" bordered column={1}>
+      <Descriptions.Item label="Project Description">Current project. Collect all five Tamas -- they each have different stories! Feed, play, and take them out. See what they're saying when they're hungry, sad, or happy. Make sure to keep up and check on them to see their happiness level or else they will die.</Descriptions.Item>
+      <Descriptions.Item label="Tech used"> JavaScript | HTML5 | CSS | Server Side API | Fetch | JSON | Node.js | Express.js | Heroku | JSON Web Tokens | MySQL | Sequelize ORM | React | React Hooks | JSX | bcrypt | dotenv | node-cron | node-fetch | sweetalert2 | howler |</Descriptions.Item>
+      <Descriptions.Item label="Date released">Sept 2021</Descriptions.Item>
+      <Descriptions.Item label="Status">
+        <Badge status='success' text='Running' />
+      </Descriptions.Item>
+    </Descriptions>
+  )
+}
 
 const ProjectCarousel: React.FC = () => {
   return (
@@ -24,17 +37,19 @@ const ProjectCarousel: React.FC = () => {
 }
 
 const ProjectGrid: React.FC = () => {
-  const { projectId } = useParams();
-
   return (
-    <Row className={styles.Row}>
+    <div style={{ overflow: 'hidden' }}>
+      <Row className={styles.Row} gutter={32} >
       <Col className={styles.ColumnPadding} span={3} />
       <Col span={18} >
         <ProjectCarousel />
-        <div>Project {projectId}</div>
+          <div>
+            <ProjectDescriptions />
+          </div>
       </Col>
       <Col className={styles.ColumnPadding} span={3} />
     </Row>
+    </div>
   )
 }
 
