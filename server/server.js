0050771b9773358854600ec12c7f7f3284d98a7b
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-09 18:15:39 -0700

    add express and sequelize config

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path'
+import sequelize from './config/index.js';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
