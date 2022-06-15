494eca9e2bfc19a581215329cfdce298891b7852
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-14 19:19:31 -0700

    add svgs to about page

--- client/src/assets/images/skill-svgs/index.ts
+++ client/src/assets/images/skill-svgs/index.ts
@@ -1,30 +1,30 @@
-import antd from './antd.svg';
-import aws from './aws.svg';
-import bootstrap from './bootstrap.svg';
-import css from './css.svg';
-import figma from './figma.svg';
-import gatsby from './gatsby.svg';
-import git from './git.svg';
-import github from './github.svg';
-import handlebars from './handlebars.svg';
-import heroku from './heroku.svg';
-import html5 from './html5.svg';
-import javascript from './javascript.svg';
-import jest from './jest.svg';
-import jwt from './jwt.svg'
-import linkedin from './linkedin.svg'
-import medium from './medium.svg'
-import mongodb from './mongodb.svg'
-import mysql2 from './mysql2.svg'
-import nodejs from './nodejs.svg'
-import postgres from './postgres.svg'
-import python from './python.svg';
-import react from './react.svg';
-import resume from './resume.svg';
-import sass from './sass.svg';
-import sequelize from './sequelize.svg';
-import sql from './sql.svg';
-import typescript from './typescript.svg'
+import AntD from './antd.svg';
+import AWS from './aws.svg';
+import Bootstrap from './bootstrap.svg';
+import CSS from './css.svg';
+import Figma from './figma.svg';
+import Gatsby from './gatsby.svg';
+import Git from './git.svg';
+import GitHub from './github.svg';
+import Handlebars from './handlebars.svg';
+import Heroku from './heroku.svg';
+import Html5 from './html5.svg';
+import Javascript from './javascript.svg';
+import Jest from './jest.svg';
+import JWT from './jwt.svg'
+import LinkedIn from './linkedin.svg'
+import Medium from './medium.svg'
+import MongoDB from './mongodb.svg'
+import MySQL from './mysql2.svg'
+import NodeJS from './nodejs.svg'
+import Postgres from './postgres.svg'
+import Python from './python.svg';
+import React from './react.svg';
+import Resume from './resume.svg';
+import SASS from './sass.svg';
+import Sequelize from './sequelize.svg';
+import SQL from './sql.svg';
+import TypeScript from './typescript.svg'
 
 type SocialSVG = {
   [key: string]: string
@@ -41,35 +41,35 @@ type BundledSVG = {
 
 const svgs: BundledSVG = {
   socials: {
-    github,
-    linkedin,
-    resume,
-    medium
+    GitHub,
+    LinkedIn,
+    Resume,
+    Medium
   },
   skills: {
-    antd,
-    aws,
-    bootstrap,
-    css,
-    figma,
-    gatsby,
-    git,
-    handlebars,
-    heroku,
-    html5,
-    javascript,
-    jest,
-    jwt,
-    mongodb,
-    mysql2,
-    nodejs,
-    postgres,
-    python,
-    react,
-    sass,
-    sequelize,
-    sql,
-    typescript
+    AntD,
+    AWS,
+    Bootstrap,
+    CSS,
+    Figma,
+    Gatsby,
+    Git,
+    Handlebars,
+    Heroku,
+    Html5,
+    Javascript,
+    Jest,
+    JWT,
+    MongoDB,
+    MySQL,
+    NodeJS,
+    Postgres,
+    Python,
+    React,
+    SASS,
+    Sequelize,
+    SQL,
+    TypeScript
   }
 }
 
