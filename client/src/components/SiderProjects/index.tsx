a87ceab1512c5ac6299e40465ca5884aa9db7529
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-15 18:05:53 -0700

    add navigate to general project sidebar

--- client/src/components/SiderProjects/index.tsx
+++ client/src/components/SiderProjects/index.tsx
@@ -2,13 +2,15 @@ import { Card } from 'antd'
 import React from 'react'
 import ProjectCards from './ProjectCardsSider'
 import { useGetUserProjects } from '../../api'
+import { useNavigate } from 'react-router-dom'
+import styles from './index.module.scss'
 
 const SiderProjects: React.FC = () => {
   const { data: projects } = useGetUserProjects();
-
+  const navigate = useNavigate();
   return (
     <>
-      <Card>Projects</Card>
+      <Card className={styles.Project} onClick={() => navigate('/projects')}>Projects</Card>
       {projects && projects.map(project => <ProjectCards key={project.id} project={project} />)}
     </>
   )
