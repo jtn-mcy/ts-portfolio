7fa04ec53526f416d7a672d781f055a157c6a4bc
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2022-06-17 14:06:17 -0700

    add me to homepage

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -2,12 +2,14 @@ import me1 from './me/me1.jpg'
 import me2 from './me/me2.png'
 import me3 from './me/me3.png'
 import me4 from './me/me4.png'
+import itme from './me/itme.png'
 import eeveeFavicon from './eeveeFavicon.png'
 import placeholderBG from './placeholderBG.png'
 import projectPlaceholder from './projectPlaceholder350x350.png'
 import svgs from './skill-svgs'
 
 export {
+  itme,
   me1,
   me2,
   me3,
